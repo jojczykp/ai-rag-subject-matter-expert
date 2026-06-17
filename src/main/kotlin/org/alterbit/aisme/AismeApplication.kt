e057638edcdb7b723e34eb4f7826019d8ea28f7a
jojczykp <pawel.jojczyk@gmail.com>
2026-06-17 21:50:27 +0200

    Rename Spring Boot application class
    Renames the application entrypoint and context-load test to AismeApplication for a shorter project-specific Spring Boot name.
    
    Verification: ./gradlew koverVerify

--- src/main/kotlin/org/alterbit/aisme/AismeApplication.kt
+++ src/main/kotlin/org/alterbit/aisme/AismeApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class AiSubjectMatterExpertApplication
+class AismeApplication
 
 fun main(args: Array<String>) {
-    runApplication<AiSubjectMatterExpertApplication>(*args)
+    runApplication<AismeApplication>(*args)
 }
--- src/test/kotlin/org/alterbit/aisme/AismeApplicationTests.kt
+++ src/test/kotlin/org/alterbit/aisme/AismeApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class AiSubjectMatterExpertApplicationTests {
+class AismeApplicationTests {
     @Test
     fun contextLoads() {
     }
